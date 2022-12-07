6254b6b1c9fc318b4bf467f392a3f5bb36d0ffdd
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-12-07 15:09:33 +0100

    getelement

---
+++ 02-DOM-Selecteurs/js/selecteur.js
@@ -0,0 +1,29 @@
+// -------------------------- LES SELECTEURS DANS JAVASCRIPT --------------------------------------------------
+
+// -------------------------- SELECTEUR NATUREL -------------------------------------------------
+let monId = masection;
+// Tous les id sont atteignables sans fonction. Je met directement le nom de l'id
+console.log(monId);
+console.log("monId : " + monId.innerHTML);
+monId.innerHTML = "Ma Super Section";
+/**
+ * innerHTML permet de selectionner le contenu compris entre les deux balises HTML de l'élément sélectionné
+ * il permet aussi de modifier ce contenu (en fait de la remplacer)
+ *
+ */
+console.log("body : " + document.body); // body est atteignable directement car il est unique dans le fichier html
+
+// -------------------------- SELECTEUR BYID -------------------------------------------------
+
+let myId = document.getElementById("masection");
+// getElementById est une fonction qui permet de sélectionner un élément HTML via son id
+console.log("myId : " + myId); // cela renvoie un object HTMLCollection (ARRAY/OBJECT)
+console.log("myId : " + myId.innerHTML);
+
+// -------------------------- SELECTEUR BYTAGNAME -------------------------------------------------
+
+let maBalise = document.getElementsByTagName("span");
+console.log("maBalise : " + maBalise); // cela renvoie un object HTMLCollection (ARRAY/OBJECT)
+console.log("maBalise : " + maBalise[0].innerHTML);
+// getElementsByTagName est une fonction qui sélectionne tous les éléments d'une page html qui pour balise
+// la balise sélectionnée. Cette fonction renvoie un résultat sous form de tableau.
