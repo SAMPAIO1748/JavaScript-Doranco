c6825837e641532b5ff622aaf7bbfd314cf1e4d9
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-12-07 16:50:51 +0100

    selecteur exo

---
+++ 02-DOM-Selecteurs/js/exo-selecteur.js
@@ -0,0 +1,19 @@
+/* Exercice : tout afficher dans la console
+  
+    - avec les trois manières possibles afficher le contenu de l'article qui a l'id monArticle
+
+    - Afficher la liste des contenues des divs qui ont la classe "mesDivs"
+
+    - Afficher le contenu du premier paragraphe (2 méthodes différentes)
+
+    - Afficher le contenu du span (2 méthodes différentes)
+
+    -Modifier le contenu du h1.
+
+    - Afficher la liste des contenus de tous les p.
+
+    - Modifier le contenu de la div qui a l'id result
+
+    - Afficher le contenu de la div qui contient 4
+
+*/
