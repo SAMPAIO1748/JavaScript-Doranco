ad3122ef6fb327eaa96e38939247e1bec49a6ca2
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2022-12-07 16:06:33 +0100

    querySelector

--- 02-DOM-Selecteurs/js/selecteur.js
+++ 02-DOM-Selecteurs/js/selecteur.js
@@ -25,8 +25,8 @@ console.log("myId : " + myId.innerHTML);
 let maBalise = document.getElementsByTagName("span");
 console.log("maBalise : " + maBalise); // cela renvoie un object HTMLCollection (ARRAY/OBJECT)
 console.log("maBalise : " + maBalise[0].innerHTML);
-// getElementsByTagName est une fonction qui sélectionne tous les éléments d'une page html qui pour balise
-// la balise sélectionnée. Cette fonction renvoie un résultat sous form de tableau.
+// getElementsByTagName est une fonction qui sélectionne tous les éléments d'une page html qui ont pour balise
+// la balise sélectionnée. Cette fonction renvoie un résultat sous forme de tableau.
 
 // -------------------------- SELECTEUR BYCLASSE -------------------------------------------------
 
@@ -41,3 +41,16 @@ for (let i = 0; i < maClasse.length; i++) {
     "Tour n°" + j + " : " + maClasse[i].innerHTML + " , id : " + maClasse[i].id
   );
 }
+// getElementsByClassName est une fonction qui sélectionne tous les éléments d'une page html qui ont pour clasee
+// la classe sélectionnée. Cette fonction renvoie un résultat sous forme de tableau.
+
+// ------------------------- SELECTION QUERYSELECTOR ---------------------------------------
+let p = document.querySelector("p"); // le querySelector récupère le premier élément qui a une balise p
+console.log("querSelector : " + p.innerHTML);
+let idSection = document.querySelector("#masection");
+// le querySelector récupère le premier élément qui a un id = "masection"
+console.log("querSelector : " + idSection.innerHTML);
+let voiture = document.querySelector(".voiture");
+// le querySelector récupère le prmeier élement qui a une classe = "voiture"
+console.log("querSelector : " + voiture.innerHTML);
+// Les sélecteurs sont les mêmes que pour CSS : . : classe, # : id
